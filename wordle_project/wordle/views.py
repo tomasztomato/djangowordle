6708b0953cceb3573cccd3671aceed6f434088ca
tomasztomato <tomasztomato1@gmail.com>
2025-01-09 11:10:06 +0100

    Hashing works
    Signed-off-by: tomasztomato <tomasztomato1@gmail.com>

--- wordle_project/wordle/views.py
+++ wordle_project/wordle/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import check_password
 
 def register_view(request):
     if request.method == 'POST':
@@ -12,23 +12,28 @@ def register_view(request):
         if User.objects.filter(username=username).exists():
             messages.error(request, 'Username already exists.')
         else:
-            hashed_password = make_password(password)
-            User.objects.create_user(username=username, password=hashed_password)
+            User.objects.create_user(username=username, password=password)
             messages.success(request, 'Registration successful! Please log in.')
             return redirect('login')
     return render(request, 'wordle/register.html')
 
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, 'Login successful!')
-            return redirect('home')
-        else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+
+            if check_password(password, user.password):
+                login(request, user)
+                messages.success(request, 'Login successful!')
+                return redirect('home')
+            else:
+                messages.error(request, 'Invalid username or password.')
+        except User.DoesNotExist:
             messages.error(request, 'Invalid username or password.')
+
     return render(request, 'wordle/login.html')
 
 def logout_view(request):
